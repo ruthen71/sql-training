dafae25c688cc992bffd629bcc94ac6abf968689
ruthen71 <takahero2016@gmail.com>
2025-08-31 00:36:47 +0900

    feat: add leetcode 577 file

---
+++ leetcode.com/0577-employee-bonus.sql
@@ -0,0 +1,11 @@
+SELECT
+    Employee.name,
+    Bonus.bonus
+FROM
+    Employee
+    LEFT OUTER JOIN Bonus
+        ON Employee.empId = Bonus.empId
+WHERE
+    Bonus.bonus IS NULL
+    OR Bonus.bonus < 1000
+;
