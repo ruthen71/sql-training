8ee75d42d79504862d6d1844d064a5b0e7d42116
ruthen71 <takahero2016@gmail.com>
2025-09-09 01:19:20 +0900

    feat: add leetcode 607 file

---
+++ leetcode.com/0607-sales-person.sql
@@ -0,0 +1,16 @@
+SELECT
+    name
+FROM
+    SalesPerson
+WHERE
+    sales_id NOT IN (
+        SELECT
+            DISTINCT Orders.sales_id
+        FROM
+            Orders
+            INNER JOIN Company
+                ON Orders.com_id = Company.com_id
+        WHERE
+            Company.name = "RED"
+    )
+;
