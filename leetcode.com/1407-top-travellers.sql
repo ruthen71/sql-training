e3847127081d3e6e3b9d2f0b87e432019e1f5368
ruthen71 <takahero2016@gmail.com>
2025-10-09 04:57:32 +0900

    feat: add leetcode 1407 file

---
+++ leetcode.com/1407-top-travellers.sql
@@ -0,0 +1,13 @@
+SELECT
+    Users.name,
+    SUM(IFNULL(Rides.distance, 0)) AS travelled_distance
+FROM
+    Users
+    LEFT OUTER JOIN Rides
+        ON Users.id = Rides.user_id
+GROUP BY
+    Users.id
+ORDER BY
+    travelled_distance DESC,
+    name ASC
+;
