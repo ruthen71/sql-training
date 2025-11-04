f4c1023d29e48cdaecb86e6d47ea350710648fb2
ruthen71 <takahero2016@gmail.com>
2025-11-05 08:44:54 +0900

    feat: add leetcode 1934 file

---
+++ leetcode.com/1934-confirmation-rate.sql
@@ -0,0 +1,10 @@
+SELECT
+    Signups.user_id,
+    ROUND(AVG(IF(action = 'confirmed', 1, 0)), 2) AS confirmation_rate
+FROM
+    Signups
+    LEFT OUTER JOIN Confirmations
+        ON Signups.user_id = Confirmations.user_id
+GROUP BY
+    Signups.user_id
+;
